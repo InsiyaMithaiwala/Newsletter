0dab292787f115c8ec7e7bb7f89133bd441494f0
insiya mithaiwala <insiyam07@gmail.com>
2023-07-13 16:00:04 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// const { log } = require("console");
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        maerge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/b2830e7c1c";
+
+  const options = {
+    method: "POST",
+    auth: "insiya1:da89a314aa55a05f97efd40d5140d3ee-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  // request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
+
+// api key
+// da89a314aa55a05f97efd40d5140d3ee-us21
+
+// list id
+// b2830e7c1c
